e8adfb750464249ac8fe3eb0a3fc4e12f8bdcc22
ELMAFOUDOUSOUSS <elmahfoud.ousouss@gmail.com>
2023-08-11 17:08:02 -0700

    tasks

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,10 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number <= 10; number++)
+	for (number = 0; number <= 9; number++)
 	{
 		putchar(number);
 	}
-	
 	putchar('\n');
 	return (0);
 }
